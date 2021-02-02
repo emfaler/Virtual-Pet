eebbc90e97d6dfb1ebaf7df8308c1928706036f5
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-02-02 11:38:05 -0500

    Added CosnoleWriteLines to Switch

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -11,30 +11,34 @@ namespace VirtualPet
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to Pixel Pet!\n");
-                Console.WriteLine("What do you want do first?");
-                Console.WriteLine("Name your pet!");
-                Console.WriteLine("Pick your pet species");
-                Console.WriteLine("1. Feed your pet?");
-                Console.WriteLine("2. Play with your pet?");
-                Console.WriteLine("3. Check your pets health!");
+                Console.WriteLine("What do you want to do first?");
+                Console.WriteLine("1. Pick your pet species");
+                Console.WriteLine("2. Name your pet!");
+                Console.WriteLine("3. Feed your pet?");
+                Console.WriteLine("4. Play with your pet?");
+                Console.WriteLine("5. Check your pets health!");
                 Console.WriteLine("Press Q to quit");
 
                 string userChoice = Console.ReadLine().ToLower();
 
                 switch (userChoice)
                 {
-                    case "1":
-                        ();
+                    case "1": 
+                       Console.WriteLine("What species of animal would you like your pet?");
                         break;
                     case "2":
-                        ();
+                        Console.WriteLine("What would you like to name your pet.");
                         break;
                     case "3":
-                        ();
+                        Console.WriteLine("Would you like to feed your pet?");
                         break;
+                    case "4":
+                        Console.WriteLine("Would you like to play with your pet?");
                         break;
-                    case "q":
-                        VirtualPet = false;
+                    case "5":
+                        Console.WriteLine("Check you pet's health.");
+                        break;
+                    case "6":
                         Console.WriteLine("Good bye!");
                         break;
                     default:
