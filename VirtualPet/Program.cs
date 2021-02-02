2cd35b03db0aeb726bf805ee7c0588c0d170ac19
Skye Blue <mitchellskye.00@gmail.com>
2021-02-02 11:08:46 -0500

    Added Choices

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -6,7 +6,39 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Welcome to Virtual Pets");
+            bool VirtualPet = true;
+            while (VirtualPet)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Pixel Pet!\n");
+                Console.WriteLine("What do you want do first?");
+                Console.WriteLine("1. Feed your pet?");
+                Console.WriteLine("2. Play with your pet?");
+                Console.WriteLine("3. Check your pets health!");
+                Console.WriteLine("Press Q to quit");
+
+                string userChoice = Console.ReadLine().ToLower();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        AreTheseIntegersEqual();
+                        break;
+                    case "2":
+                        EvenOrOdd();
+                        break;
+                    case "3":
+                        VowelOrConsonant();
+                        break;
+                        break;
+                    case "q":
+                        keepThinking = false;
+                        Console.WriteLine("Good bye!");
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
