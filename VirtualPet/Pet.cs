3392edbce5a56908a734f0265cbc9be9fcf1d5f1
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-02-02 11:00:07 -0500

    As a team we were able to complete the test for the project. We were able to go over questions about what was done and properly finish this segment of the project.

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -6,5 +6,86 @@ namespace VirtualPet
 {
     public class Pet
     {
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public int Hunger { get; set; } = 50;
+
+        public int Boredom { get; set; } = 60;
+
+        public int Health { get; set; } = 30;
+
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+        }
+
+        public string GetSpecies()
+        {
+            return Species;
+        }
+
+        public int GetHunger()
+        {
+            return Hunger;
+        }
+
+        public int GetBoredom()
+        {
+            return Boredom;
+        }
+
+        public int GetHealth()
+        {
+            return Health;
+        }
+
+        public void Feed()
+        {
+            Hunger -= 40;
+        }
+
+        public void SeeDoctor()
+        {
+            Health += 30;
+        }
+
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+        }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
