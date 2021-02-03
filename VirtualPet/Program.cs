a11d5c762b82fba0c6e22da063e6864af5dab6b3
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-02-03 14:03:26 -0500

    Added user exit, deleted comments, added random status integers

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,15 +7,15 @@ namespace VirtualPet
         static void Main(string[] args)
         {
 
-            //Random random = new Random();
-            //int startHunger = random.Next(50, 70);
-            //int startBoredom = random.Next(50, 60);
-            //int startHealth = random.Next(30, 50);
+            Random random = new Random();
+            int startHunger = random.Next(50, 70);
+            int startBoredom = random.Next(50, 60);
+            int startHealth = random.Next(30, 50);
 
             Pet pet = new Pet();
-            //pet.Hunger = startHunger;           
-            //pet.Boredom = startBoredom;
-            //pet.Health = startHealth;             
+            pet.Hunger = startHunger;
+            pet.Boredom = startBoredom;
+            pet.Health = startHealth;
 
             bool VirtualPet = true;
 
@@ -29,6 +29,7 @@ namespace VirtualPet
                 Console.WriteLine("4. Play with your pet");
                 Console.WriteLine("5. Take pet to the doctor");
                 Console.WriteLine("6. Check the status of you pet");
+                Console.WriteLine("7. Exit game.");
 
                 string userChoice = Console.ReadLine().ToLower();
                 Console.Clear();
@@ -36,29 +37,30 @@ namespace VirtualPet
                 switch (userChoice)
                 {
                     case "1":
-                        Console.WriteLine("What species of animal would you like?");                // Species is done!
+                        Console.WriteLine("What species of animal would you like?");                
                         string speciesOfPet = Console.ReadLine();
                         pet.SetSpecies(speciesOfPet);
                         Console.WriteLine($"The species of your animal is: {pet.Species}");
                         break;
                     case "2":
-                        Console.WriteLine("What would you like to name your pet?");                // Pet name is done!
+                        Console.WriteLine("What would you like to name your pet?");                
                         string nameOfPet = Console.ReadLine();
                         pet.SetName(nameOfPet);
                         Console.WriteLine($"Congratulations! \nThe name of your pet is: {pet.Name}");
                         break;
                     case "3":
+                        //Console.WriteLine( $"How much would you like to feed {pet.Name}");
+                        //int userFeed = Convert.ToInt32(Console.ReadLine());
                         pet.Feed();
                         break;
                     case "4":
-                        Console.WriteLine("Play with your pet");
-                        Console.WriteLine($"{pet.Boredom}");
+                        pet.Play();
                         break;
                     case "5":
-                        Console.WriteLine("Take pet to the doctor");
+                        pet.SeeDoctor();
                         break;
                     case "6":
-                        Console.WriteLine("Check overall status of pet:");
+                        Console.WriteLine("Check overall status of pet:\n");
                         Console.WriteLine($"The name of your pet is {pet.Name} with a species type of {pet.Species}");                     
                         Console.WriteLine($"Hunger: {pet.Hunger}");
                         Console.WriteLine($"Boredom: {pet.Boredom}");
@@ -69,6 +71,7 @@ namespace VirtualPet
                         Console.WriteLine("Good bye!");
                         break;
                     default:
+                        Console.WriteLine("Please enter a valid number.");
                         break;
                 }
 
