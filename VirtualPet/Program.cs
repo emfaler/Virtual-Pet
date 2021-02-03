93a5488300b69bf1ef0dccc95818887469fc99aa
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-02-02 20:14:31 -0500

    added a random class to have the stats of our pet pop up when everything starts running. started working on a menu to select what pet they would want to interact with.

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -6,13 +6,36 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+
+            Random random = new Random();
+            int startHunger = random.Next(50);
+            int startThirst = random.Next(60);
+            int startEnergy = random.Next(70);
+
             Pet pet = new Pet();
+            pet.Hunger = startHunger;
+            pet.Thirst = startThirst;               // create methods for thirst  set thrist to 60 inital
+            pet.Energy = startEnergy;               // create methods for energy  set energy to 70 inital 
+
+
+            //List<Pet> animalType = new List<Pet>();
+            //animalType.Add("Dog");
+            //animalType.Add("Cat");
+            //animalType.Add("Bird");
+
+            //for (int i = 0; i < animalType.Count; i++)
+            //{
+            //    int menuNumber = i + 1;
+            //    Console.WriteLine("\tPress {0} for {1}.", menuNumber, animalType[i]);
+            //}
+
+
             bool VirtualPet = true;
 
             while (VirtualPet)
             {
                 Console.WriteLine("Welcome to Pixel Pet!\n");
-                Console.WriteLine("What do you want to do first?");
+                Console.WriteLine("What do you want to do?");
                 Console.WriteLine("1. Pick your pet species");
                 Console.WriteLine("2. Name your pet!");
                 Console.WriteLine("3. Feed your pet?");
@@ -37,9 +60,9 @@ namespace VirtualPet
                         Console.WriteLine($"Congratulations! \nThe name of your pet is: {pet.Name}");
                         break;
                     case "3":
-                        Console.WriteLine("Would you like to feed your pet?");                             // Need to create a method to feed animal with string
-                        string petFood = Console.ReadLine();
-                      //  pet.(petFood);
+                        Console.WriteLine("What would you like to feed your pet?");                        // Need to create a method to feed animal with string
+                        int petFood = Convert.ToInt32(Console.ReadLine());
+                        // pet.(petFood);
                         break;
                     case "4":
                         Console.WriteLine("What activity would you like to do with your pet?");            // Need to create method for activity with string 
