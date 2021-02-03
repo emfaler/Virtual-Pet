afe04d766035372878633f4a1f5b3d7385d1531e
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-02-03 13:42:55 -0500

    Was able to add more console writeline so the application works more like a game. added the do while loop for the game. Able to add a check status function so the user can see the name, species and status levels of their pet.

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,27 +7,28 @@ namespace VirtualPet
         static void Main(string[] args)
         {
 
-            Random random = new Random();
-            int startHunger = random.Next(50);
-            int startEnergy = random.Next(70);
-            int startHealth = random.Next(30);
+            //Random random = new Random();
+            //int startHunger = random.Next(50, 70);
+            //int startBoredom = random.Next(50, 60);
+            //int startHealth = random.Next(30, 50);
 
             Pet pet = new Pet();
-            pet.Hunger = startHunger;           // create methods for thirst  set thrist to 60 inital
-            pet.Boredom = startEnergy;
-            pet.Health = startHealth;              // create methods for energy  set energy to 70 inital 
+            //pet.Hunger = startHunger;           
+            //pet.Boredom = startBoredom;
+            //pet.Health = startHealth;             
 
             bool VirtualPet = true;
 
-            while (VirtualPet)
+            do
             {
                 Console.WriteLine("Welcome to Pixel Pet!\n");
                 Console.WriteLine("What do you want to do?");
                 Console.WriteLine("1. Pick your pet species");
                 Console.WriteLine("2. Name your pet!");
-                Console.WriteLine("3. Feed your pet?");
-                Console.WriteLine("4. Play with your pet?");
-                Console.WriteLine("5. Check your pets health!");
+                Console.WriteLine("3. Feed your pet");
+                Console.WriteLine("4. Play with your pet");
+                Console.WriteLine("5. Take pet to the doctor");
+                Console.WriteLine("6. Check the status of you pet");
 
                 string userChoice = Console.ReadLine().ToLower();
                 Console.Clear();
@@ -39,54 +40,43 @@ namespace VirtualPet
                         string speciesOfPet = Console.ReadLine();
                         pet.SetSpecies(speciesOfPet);
                         Console.WriteLine($"The species of your animal is: {pet.Species}");
-
-                        Console.WriteLine("\nReturn to Main Menu.");
-                        Console.ReadKey();
-                        Console.Clear();
                         break;
                     case "2":
-                        Console.WriteLine("What would you like to name your pet?");                         // Pet name is done!
+                        Console.WriteLine("What would you like to name your pet?");                // Pet name is done!
                         string nameOfPet = Console.ReadLine();
                         pet.SetName(nameOfPet);
                         Console.WriteLine($"Congratulations! \nThe name of your pet is: {pet.Name}");
-
-                        Console.WriteLine("\nReturn to Main Menu.");
-                        Console.ReadKey();
-                        Console.Clear();
                         break;
                     case "3":
-                        Console.WriteLine("What would you like to feed your pet?");
-                        Console.WriteLine($"{pet.Hunger }");
-                        Console.WriteLine("\nReturn to Main Menu.");
-                        Console.ReadKey();
-                        Console.Clear();                                   // Need to create a method to feed animal with string
-
-                        // pet.(petFood);
+                        pet.Feed();
                         break;
                     case "4":
-                        Console.WriteLine("What activity would you like to do with your pet?");
+                        Console.WriteLine("Play with your pet");
                         Console.WriteLine($"{pet.Boredom}");
-                        Console.WriteLine("\nReturn to Main Menu.");                                        //change the place
-                        Console.ReadKey();
-                        Console.Clear();
                         break;
                     case "5":
-                        pet.GetHealth();
-                        Console.WriteLine("Pet's health:");
-                        Console.WriteLine($"{pet.Health}");
-                        Console.WriteLine("\nReturn to Main Menu.");
-                        Console.ReadKey();
-                        Console.Clear();
-
+                        Console.WriteLine("Take pet to the doctor");
                         break;
                     case "6":
+                        Console.WriteLine("Check overall status of pet:");
+                        Console.WriteLine($"The name of your pet is {pet.Name} with a species type of {pet.Species}");                     
+                        Console.WriteLine($"Hunger: {pet.Hunger}");
+                        Console.WriteLine($"Boredom: {pet.Boredom}");
+                        Console.WriteLine($"Health: {pet.Health}");
+                        break;
+                    case "7":
+                        VirtualPet = false;
                         Console.WriteLine("Good bye!");
                         break;
                     default:
                         break;
                 }
 
-            }
+                Console.WriteLine("Return to main menu");
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (VirtualPet);
         }
     }
 }
