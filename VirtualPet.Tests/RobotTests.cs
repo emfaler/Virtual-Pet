a4b2a69db7a937f7665a5d3150cd012050050d0b
Skye Blue <mitchellskye.00@gmail.com>
2021-02-04 14:04:20 -0500

    Robot and Organic test

--- VirtualPet.Tests/RobotTests.cs
+++ VirtualPet.Tests/RobotTests.cs
@@ -14,6 +14,119 @@ namespace VirtualPet.Tests
             robotPets = new RobotPet();
         }
 
+        [Fact]
+        public void Pet_Should_Have_GasLevel()
+        {
+            testPet.GasLevel = 100;
+
+            Assert.Equal(100, testPet.GasLevel);
+        }
+
+        [Fact]
+        public void GetGasLevel_Should_Return_Initial_GasLevel_Level_Of_50()
+        {
+            int testPetGasLevel = testPet.GetGasLevel();
+
+            Assert.Equal(50, testPetGasLevel);
+        }
+
+        [Fact]
+        public void Pet_Should_Have_Charger()
+        {
+            testPet.Charger = 100;
+
+            Assert.Equal(100, testPet.Charger);
+        }
+
+        [Fact]
+        public void GetCharger_Should_Return_Initial_Charger_Level_Of_60()
+        {
+            int testPetCharger = testPet.GetCharger();
+
+            Assert.Equal(60, testPetCharger);
+        }
+
+        [Fact]
+        public void Pet_Should_Have_OilLevel()
+        {
+            testPet.OilLevel = 100;
+
+            Assert.Equal(100, testPet.OilLevel);
+        }
+
+        [Fact]
+        public void GetOilLevel_Should_Return_Initial_OilLevel_Level_Of_30()
+        {
+            int testPetHealth = testPet.GetOilLevel();
+
+            Assert.Equal(30, testPetOilLevel);
+        }
+
+        [Fact]
+        public void Gas_Should_Decrease_GasLevel_By_40()
+        {
+            testPet.Gas();
+
+            Assert.Equal(10, testPet.GetGasLevel());
+        }
+
+        [Fact]
+        public void SeeMechanic_Should_Increase_OilLevel_By_30()
+        {
+            testPet.SeeMechanic();
+
+            Assert.Equal(60, testPet.GetOilLevel());
+        }
+
+        [Fact]
+        public void Play_Should_Increase_GasLevel_By_10()
+        {
+            testPet.Play();
+
+            Assert.Equal(60, testPet.GetGasLevel());
+        }
+
+        [Fact]
+        public void Play_Should_Decrease_Charger_By_20()
+        {
+            testPet.Play();
+
+            Assert.Equal(40, testPet.GetCharger());
+        }
+
+        [Fact]
+        public void Play_Should_Increase_OilLevel_By_10()
+        {
+            testPet.Play();
+
+            Assert.Equal(40, testPet.GetOilLevel());
+        }
+
+        [Fact]
+        public void Tick_Should_Increase_GasLevel_By_5()
+        {
+            testPet.Tick();
+
+            Assert.Equal(55, testPet.GetGasLevel());
+        }
+
+        [Fact]
+        public void Tick_Should_Increase_Charger_By_5()
+        {
+            testPet.Tick();
+
+            Assert.Equal(65, testPet.GetCharger());
+        }
+
+        [Fact]
+        public void Tick_Should_Decrease_OilLevel_By_5()
+        {
+            testPet.Tick();
+
+            Assert.Equal(25, testPet.GetOilLevel());
+        }
+    }
+}
 
 
 
--- VirtualPet/RobotPet.cs
+++ VirtualPet/RobotPet.cs
@@ -6,26 +6,94 @@ namespace VirtualPet
 {
     public class RobotPet : Pet
     {
+        public int GasLevel { get; set; } = 50;   //hunger
 
-       //public int Charge { get; set; }
+        public int Charger { get; set; } = 60;  //Energy
 
-       //public int Boredom { get; set; }
+        public int OilLevel { get; set; } = 30;   //health
+
+        public int GetGasLevel()
+        {
+            return GasLevel;
+        }
+
+        public int GetCharger()
+        {
+            return Charger;
+        }
+
+        public int GetOilLevel()
+        {
+            return OilLevel;
+        }
+
+        public void Gas()
+        {
+            GasLevel -= 40;
+
+            if (GasLevel < 0)
+            {
+                GasLevel = 0;
+            }
+        }
+
+        public void SeeMechanic()
+        {
+            OilLevel += 30;
+
+            if (OilLevel > 100)
+            {
+                OilLevel = 100;
+            }
+        }
+
+        public void Play()
+        {
+            GasLevel += 10;
+            Charger -= 20;
+            OilLevel += 10;
+
+            if (GasLevel > 100)
+            {
+                GasLevel = 100;
+            }
+
+            if (Charger < 0)
+            {
+                Charger = 0;
+            }
+
+            if (OilLevel > 100)
+            {
+                OilLevel = 100;
+            }
 
 
+        }
 
+        public void Tick()
+        {
+            GasLevel += 5;
+            Charger += 5;
+            OilLevel -= 5;
 
+            if (GasLevel > 100)
+            {
+                GasLevel = 100;
+            }
+
+            if (Charger > 100)
+            {
+                Charger = 100;
+            }
+
+            if (OilLevel < 0)
+            {
+                OilLevel = 0;
+            }
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
+}
 
     //public void Feed()
     //{
