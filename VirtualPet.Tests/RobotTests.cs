711e0e870a56b0d0898b60e850adc9b6c742ec8f
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-02-03 19:31:33 -0500

    Was able to start iteration 3 with the robots vs organic pet. Created the test class and program class for robots.

---
+++ VirtualPet.Tests/RobotTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VirtualPet.Tests
+{
+    public class RobotTests
+    {
+        Robots robotPets; 
+        
+        public RobotTests()
+        {
+            robotPets = new Robots();
+        }
+        
+        
+        
+        [Fact]
+        public void 
+
+
+
+
+
+
+    }
+}
