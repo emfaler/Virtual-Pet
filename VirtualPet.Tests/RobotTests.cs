e303da7b72e6db98e8981ad803379312ed339245
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-02-04 14:11:29 -0500

    Corrected Robot Tests

--- VirtualPet.Tests/RobotTests.cs
+++ VirtualPet.Tests/RobotTests.cs
@@ -17,15 +17,15 @@ namespace VirtualPet.Tests
         [Fact]
         public void Pet_Should_Have_GasLevel()
         {
-            testPet.GasLevel = 100;
+            robotPets.GasLevel = 100;
 
-            Assert.Equal(100, testPet.GasLevel);
+            Assert.Equal(100, robotPets.GasLevel);
         }
 
         [Fact]
         public void GetGasLevel_Should_Return_Initial_GasLevel_Level_Of_50()
         {
-            int testPetGasLevel = testPet.GetGasLevel();
+            int testPetGasLevel = robotPets.GetGasLevel();
 
             Assert.Equal(50, testPetGasLevel);
         }
@@ -33,15 +33,15 @@ namespace VirtualPet.Tests
         [Fact]
         public void Pet_Should_Have_Charger()
         {
-            testPet.Charger = 100;
+            robotPets.Charger = 100;
 
-            Assert.Equal(100, testPet.Charger);
+            Assert.Equal(100, robotPets.Charger);
         }
 
         [Fact]
         public void GetCharger_Should_Return_Initial_Charger_Level_Of_60()
         {
-            int testPetCharger = testPet.GetCharger();
+            int testPetCharger = robotPets.GetCharger();
 
             Assert.Equal(60, testPetCharger);
         }
@@ -49,81 +49,81 @@ namespace VirtualPet.Tests
         [Fact]
         public void Pet_Should_Have_OilLevel()
         {
-            testPet.OilLevel = 100;
+            robotPets.OilLevel = 100;
 
-            Assert.Equal(100, testPet.OilLevel);
+            Assert.Equal(100, robotPets.OilLevel);
         }
 
         [Fact]
         public void GetOilLevel_Should_Return_Initial_OilLevel_Level_Of_30()
         {
-            int testPetHealth = testPet.GetOilLevel();
+            int testPetHealth = robotPets.GetOilLevel();
 
-            Assert.Equal(30, testPetOilLevel);
+            Assert.Equal(30, robotPets.OilLevel);
         }
 
         [Fact]
         public void Gas_Should_Decrease_GasLevel_By_40()
         {
-            testPet.Gas();
+            robotPets.Gas();
 
-            Assert.Equal(10, testPet.GetGasLevel());
+            Assert.Equal(10, robotPets.GetGasLevel());
         }
 
         [Fact]
         public void SeeMechanic_Should_Increase_OilLevel_By_30()
         {
-            testPet.SeeMechanic();
+            robotPets.SeeMechanic();
 
-            Assert.Equal(60, testPet.GetOilLevel());
+            Assert.Equal(60, robotPets.GetOilLevel());
         }
 
         [Fact]
         public void Play_Should_Increase_GasLevel_By_10()
         {
-            testPet.Play();
+            robotPets.Play();
 
-            Assert.Equal(60, testPet.GetGasLevel());
+            Assert.Equal(60, robotPets.GetGasLevel());
         }
 
         [Fact]
         public void Play_Should_Decrease_Charger_By_20()
         {
-            testPet.Play();
+            robotPets.Play();
 
-            Assert.Equal(40, testPet.GetCharger());
+            Assert.Equal(40, robotPets.GetCharger());
         }
 
         [Fact]
         public void Play_Should_Increase_OilLevel_By_10()
         {
-            testPet.Play();
+            robotPets.Play();
 
-            Assert.Equal(40, testPet.GetOilLevel());
+            Assert.Equal(40, robotPets.GetOilLevel());
         }
 
         [Fact]
         public void Tick_Should_Increase_GasLevel_By_5()
         {
-            testPet.Tick();
+            robotPets.Tick();
 
-            Assert.Equal(55, testPet.GetGasLevel());
+            Assert.Equal(55, robotPets.GetGasLevel());
         }
 
         [Fact]
         public void Tick_Should_Increase_Charger_By_5()
         {
-            testPet.Tick();
+            robotPets.Tick();
 
-            Assert.Equal(65, testPet.GetCharger());
+            Assert.Equal(65, robotPets.GetCharger());
         }
 
         [Fact]
         public void Tick_Should_Decrease_OilLevel_By_5()
         {
-            testPet.Tick();
+            robotPets.Tick();
 
-            Assert.Equal(25, testPet.GetOilLevel());
+            Assert.Equal(25, robotPets.GetOilLevel());
         }
     }
 }
